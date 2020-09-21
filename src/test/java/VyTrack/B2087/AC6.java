cc492fa76dde4ad3563667fed68a64c5d036ae0e
Volodymyr0077 <68976794+Volodymyr0077@users.noreply.github.com>
2020-09-21 15:35:41 -0500

    Update AC6.java

--- src/test/java/VyTrack/B2087/AC6.java
+++ src/test/java/VyTrack/B2087/AC6.java
@@ -1,11 +1,13 @@
 package VyTrack.B2087;
 
+import com.sun.org.apache.xpath.internal.operations.And;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -15,6 +17,7 @@ import java.util.concurrent.TimeUnit;
 public class AC6 {
 
     WebDriver driver;
+    WebElement gridSettings;
 
     @BeforeMethod
 
@@ -22,34 +25,46 @@ public class AC6 {
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(12, TimeUnit.SECONDS);
-    }
-    @Test
-    public void VtTrackTest1() throws InterruptedException {
         driver.get("https://qa2.vytrack.com/");
         WebElement userName = driver.findElement(By.id("prependedInput"));
         userName.sendKeys("user160");
         WebElement passWord = driver.findElement(By.id("prependedInput2"));
         passWord.sendKeys("UserUser123" + Keys.ENTER);
-
+    }
+    @Test
+    //Given I am on Vehicles Odometer page as a Truck Driver
+    public void test1() throws InterruptedException {
 
         WebElement fleetElement = driver.findElement(By.cssSelector("span[class='title title-level-1']"));
         fleetElement.click();//clicking on Fleet button
-        WebElement vehicleOdometer=driver.findElement(By.linkText("Vehicle Odometer"));
+        WebElement vehicleOdometer = driver.findElement(By.linkText("Vehicle Odometer"));
         vehicleOdometer.click();//clicking on Vehicle odometer button
-        WebElement gridSettings=driver.findElement(By.xpath("//a[@title='Grid Settings']"));
+
+        // When I click on Grid settings symbol on the right top of the grid
+
+        gridSettings = driver.findElement(By.xpath("//a[@title='Grid Settings']"));
         gridSettings.click();//clicking on a grit settings button
 
-      List<WebElement>gridSetting= driver.findElements(By.xpath("//*[contains(@id,'column')]"));// list of grid setting buttons
+        //And select/deselect the properties of the odometer as I want it to be displayed
 
-      for(WebElement each:gridSetting){//clicking on every setting button
-          Thread.sleep(500);
+        List<WebElement> gridSetting = driver.findElements(By.xpath("//*[contains(@id,'column')]"));// list of grid setting buttons
+
+        for (WebElement each : gridSetting) {//clicking on every setting button
+            Thread.sleep(500);
             each.click();
         }
 
+        WebElement idColumn=driver.findElement(By.xpath("//span[@class='grid-header-cell__label']"));// header Column
+
+        String ColumnTestExpected="ID";
+        String ColumnTextActual = idColumn.getText();
+
+        Assert.assertEquals(ColumnTextActual, ColumnTestExpected);
+        gridSettings.click();//clicking on a grit settings button
+
+        WebElement resetButton= driver.findElement(By.linkText("Reset"));
+        resetButton.click();// clicking on reset button
+
 
     }
-
-
-
-
 }
